ddeae8515fd6b6035a23ab917ef56b65e806491e
mrcs22 <marcusmoraes2010@hotmail.com>
2021-05-11 18:25:44 -0300

    Requisitando conteudo da pagina inicial via axios

--- src/components/movieList/MoviesList.js
+++ src/components/movieList/MoviesList.js
@@ -1,15 +1,26 @@
+import { useEffect, useState } from "react";
+import axios from "axios";
 import List from "../general/FlexUl";
 import MovieCard from "./MovieCard";
 
 export default function MoviesList() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const promise = axios.get(
+      "https://mock-api.bootcamp.respondeai.com.br/api/v2/cineflex/movies"
+    );
+
+    promise.then((res) => {
+      setMovies(res.data);
+    });
+  }, []);
+
   return (
     <List centered>
-      {/* Just for now*/}
-      <MovieCard imageSrc="https://www.cineplayers.com/sites/default/files/styles/poster/public/posters/488_poster_0.jpg?itok=lJBujj0k" />
-      <MovieCard imageSrc="https://www.cineplayers.com/sites/default/files/styles/poster/public/posters/488_poster_0.jpg?itok=lJBujj0k" />{" "}
-      <MovieCard imageSrc="https://www.cineplayers.com/sites/default/files/styles/poster/public/posters/488_poster_0.jpg?itok=lJBujj0k" />{" "}
-      <MovieCard imageSrc="https://www.cineplayers.com/sites/default/files/styles/poster/public/posters/488_poster_0.jpg?itok=lJBujj0k" />{" "}
-      {/* Just for now*/}
+      {movies.map(({ posterURL, id }) => (
+        <MovieCard key={id} imageSrc={posterURL} />
+      ))}
     </List>
   );
 }
