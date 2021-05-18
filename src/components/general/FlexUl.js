4ac6ee39c3dfa8e453fd9a2ba8fd645fe28505f6
ronald <ronaldjunior2@hotmail.com>
2021-05-18 16:17:17 -0300

    preparando review

--- src/components/general/FlexUl.js
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export default styled.ul`
-  display: flex;
-  justify-content: ${({ centered }) => (centered ? "center" : "left")};
-  flex-wrap: wrap;
-  list-style: none;
-`;
